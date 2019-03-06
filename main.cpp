6e9dffd18940edf851340dc6382f1955ae296f5d
Joerg Boehme <joerg@chaosdorf.de>
2019-03-06 14:29:36 +0100

    Signals example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 
-#include <boost/filesystem.hpp>
+#include <boost/optional.hpp>
+#include <boost/signals2.hpp>
+
+class Foo {};
 
 int main()
 {
-    std::cout << "Hello boost\n";
+  boost::signals2::signal<Foo(bool)> s{};
+  boost::signals2::connection c = s.connect(
+    [] (bool isThrow) -> Foo {
 
-    return 0;
+      std::cout << "Hello, world!\n";
+
+      if(isThrow)
+        throw std::exception{};
+
+      return Foo{};
+    });
+
+  try {
+    boost::optional<Foo> r = s(true);
+    if(r)
+    {
+      Foo f = r.get();
+      std::cout << "Hello, Foo!\n";
+    }
+    else
+    {
+      std::cout << "Empty\n";
+    }
+  }
+  catch(std::exception& ex)
+  {
+    std::cout << "Exception is thrown!\n";
+  }
+  c.disconnect();
 }
