e8a7700087345fe7b2f5f442d79dccbce968527a
Joerg Boehme <joerg@chaosdorf.de>
2019-08-26 12:00:06 +0200

    Add program options example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
+#include <boost/program_options.hpp>
 #include <iostream>
 
-#include <boost/filesystem.hpp>
+using namespace boost::program_options;
 
-int main()
+void on_age(int age)
 {
-    std::cout << "Hello boost\n";
+  std::cout << "On age: " << age << '\n';
+}
 
-    return 0;
+int main(int argc, const char *argv[])
+{
+  try
+  {
+    options_description desc{"Options"};
+    desc.add_options()
+      ("help,h", "Help screen")
+      ("pi", value<float>()->default_value(3.14f), "Pi")
+      ("age", value<int>()->notifier(on_age), "Age");
+
+    variables_map vm;
+    store(parse_command_line(argc, argv, desc), vm);
+    notify(vm);
+
+    if (vm.count("help"))
+      std::cout << desc << '\n';
+    else if (vm.count("age"))
+      std::cout << "Age: " << vm["age"].as<int>() << '\n';
+    else if (vm.count("pi"))
+      std::cout << "Pi: " << vm["pi"].as<float>() << '\n';
+  }
+  catch (const error &ex)
+  {
+    std::cerr << ex.what() << '\n';
+  }
 }
