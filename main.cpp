670e3139e092c35bb3d73cb1c4728e513c744dfb
Joerg Boehme <joerg@chaosdorf.de>
2019-10-14 11:34:17 +0200

    Add boost asio dispatch example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 
-#include <boost/filesystem.hpp>
+#include <boost/asio.hpp>
+
+void helloFunction()
+{
+  std::cout << "Hello function\n";
+}
 
 int main()
 {
-    std::cout << "Hello boost\n";
+  std::cout << "Hello boost\n";
 
-    return 0;
+  boost::asio::io_context ioContext;
+
+  std::cout << "Dispatch\n";
+  boost::asio::dispatch(ioContext, helloFunction);
+
+  std::cout << "run\n";
+  ioContext.run();
+
+  std::cout << "End\n";
+  return 0;
 }
